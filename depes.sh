171c4e3f5af16f81addbb3e9adfa7b8576eb64e4
S4NTR1 <63378174+S4NTR1@users.noreply.github.com>
2020-04-09 07:08:14 +0700

    baru
    Silahkan di coba

---
+++ depes.sh
@@ -0,0 +1,54 @@
+#!bin/sh
+
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+clear
+echo $me"████████╗███████╗██████╗  █████╗ ███████╗    ██╗███╗   ██╗██████╗ ███████╗██╗  ██╗"
+echo $me"╚══██╔══╝██╔════╝██╔══██╗██╔══██╗██╔════╝    ██║████╗  ██║██╔══██╗██╔════╝╚██╗██╔╝"
+echo $me"   ██║   █████╗  ██████╔╝███████║███████╗    ██║██╔██╗ ██║██║  ██║█████╗   ╚███╔╝ "
+echo $pu"   ██║   ██╔══╝  ██╔══██╗██╔══██║╚════██║    ██║██║╚██╗██║██║  ██║██╔══╝   ██╔██╗ "
+echo $pu"   ██║   ███████╗██████╔╝██║  ██║███████║    ██║██║ ╚████║██████╔╝███████╗██╔╝ ██╗"
+echo $pu"   ╚═╝   ╚══════╝╚═════╝ ╚═╝  ╚═╝╚══════╝    ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝"
+echo $i"═════════════════════════════════════════════════════════════════════════════════════"
+echo $i" ╔══════════════════════════════════════════════════════════════════════════════════╗"
+echo $i" ║                       Author   :   S4NTR1 T3RS4K1T1                              ║"
+echo $i" ║                       GitHub   :   https://github.com/S4NTR1                     ║"
+echo $i" ║                       IG       :   nhill_03                                      ║"
+echo $i" ╚══════════════════════════════════════════════════════════════════════════════════╝"
+echo ""
+echo $ku"Copas Link Ini Di Browser Jika Anda Tidak Mengetahui Passwordnya"
+echo""
+sleep 2
+echo $cy"[             https://adsafelink.com/6crOHO53C                 ]"
+echo""
+sleep 2
+echo $me""$cy"["    $me"CHAT ADMIN JIKA LINK DOWNLOAD EROR"$cy "] WA = "$i"[   085755351478   ]"
+echo""
+sleep 2
+echo $pur"ketik "$me"tutor "$pur"jika anda tidak tahu cara download link nya.."
+echo""
+read -p "Masukkan Password ==> " pass
+if [ $pass = NILKAHIS151011 ]; then
+     echo $i"Login Sukses..."
+     sleep 2
+     sh bner.sh
+elif [ $pass = tutor ]; then
+     sleep 1
+     echo""
+     echo $ku"Lihat Tutorialnya Di Video Berikut Ini"
+     sleep 2
+     xdg-open https://youtu.be/ewFZ3wNfRFI
+     sh depes.sh
+else
+     echo $me"Salah Goblok..!!!"
+     sleep 1
+     echo $ku"Ulangi Lagi..."
+     sleep 2
+     sh depes.sh
+fi
